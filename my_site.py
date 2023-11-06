d0081b3d84954a357198a1a8c96fdfdbf7eea543
Nina Moisienko <148461033+NinaMoisienko18@users.noreply.github.com>
2023-11-06 15:57:28 +0200

    Update my_site.py

--- my_site.py
+++ my_site.py
@@ -44,7 +44,7 @@ if selected =="Analysis of world religions":
     with col1:
         st.write(' ')
     with col2:
-        st.image("stock-religions.jpeg")
+        st.image("dataset/stock-religions.jpeg")
     with col3:
         st.write(' ')
         
@@ -66,7 +66,7 @@ if selected =="Analysis of world religions":
     st.write("I selected this dataset to analyse the global spread of religions and forecast changes over the next 30 years. The aim is to examine the evolution of the global religious composition over time")
 
     
-    df = pd.read_csv("rounded_population.csv")
+    df = pd.read_csv("dataset/rounded_population.csv")
     st.write(df)
     st.subheader("To start, we filtered the data and selected rows where Region equals World")
     
@@ -272,7 +272,7 @@ if selected =="Analysis of world religions":
     st.write("")
     
     
-    with open('Religios.txt', 'r') as f:
+    with open('dataset/Religios.txt', 'r') as f:
         religios_text = f.read()
         
     stopwords = set(STOPWORDS)
@@ -342,7 +342,7 @@ if selected == "Analysis of space travelers":
     with col1:
         st.write(' ')
     with col2:
-        st.image("astronauts.jpeg", width=600)
+        st.image("dataset_space/astronauts.jpeg", width=600)
     with col3:
         st.write(' ')
         
@@ -367,15 +367,35 @@ if selected == "Analysis of space travelers":
 
 
 
-    dataset_space = pd.read_csv("dataset_anls.csv", delimiter=";")
+    dataset_space = pd.read_csv("dataset_space/dataset_anls.csv", delimiter=";")
     st.write(dataset_space)
     st.write("")
     st.write("")
+    
+    dataset_space["Date"] = pd.to_datetime(dataset_space["Date"], format="%d.%m.%Y")
+
+    dataset_space["Year"] = dataset_space["Date"].dt.year
+    new_dataframe_1 = dataset_space.drop(columns=["Date"]).copy()
+    new_dataframe_1 = new_dataframe_1.rename(columns={"Year": "Date"})
+    
+    with st.sidebar:
+        st.write("")
+  
+        st.write('**Selecting a range of years**')
+        min_year = new_dataframe_1['Date'].min()
+        max_year = new_dataframe_1['Date'].max()
+        start_year, end_year = st.slider('Select the start and end years',
+                                        min_value=min_year,
+                                        max_value=max_year,
+                                        value=(min_year, max_year))
+
+
+    new_dataframe = new_dataframe_1[(new_dataframe_1['Date'] >= start_year) & (new_dataframe_1['Date'] <= end_year)]
+    
+    
     st.subheader("Create interactive graph for count of flights for each country")
-    country_flight_count = dataset_space.groupby("Launching Country")["Flight "].nunique() # nunique() - return the number of these unique values
+    country_flight_count = new_dataframe.groupby("Launching Country")["Flight "].nunique() # nunique() - return the number of these unique values
     sorted_data = country_flight_count.sort_values(ascending=False) #sort values from higher to lower
-
-
 
 
 
@@ -403,20 +423,6 @@ if selected == "Analysis of space travelers":
     new_dataframe_1 = new_dataframe_1.rename(columns={"Year": "Date"})
     
     
-
-    with st.sidebar:
-        st.write("")
-  
-        st.write('**Selecting a range of years**')
-        min_year = new_dataframe_1['Date'].min()
-        max_year = new_dataframe_1['Date'].max()
-        start_year, end_year = st.slider('Select the start and end years',
-                                        min_value=min_year,
-                                        max_value=max_year,
-                                        value=(min_year, max_year))
-
-
-    new_dataframe = new_dataframe_1[(new_dataframe_1['Date'] >= start_year) & (new_dataframe_1['Date'] <= end_year)]
 
 
     years_flight_count = new_dataframe.groupby("Date")["Flight "].nunique()
